afd7e37a756e0b41c2c675db0f000aac7c2795cb
virakkhun <virak.dev.2000@gmail.com>
2024-05-16 20:41:39 +0700

    chore: fix sensetive data leak

--- src/app/_infrastructure/apis/weather.api.ts
+++ src/app/_infrastructure/apis/weather.api.ts
@@ -2,8 +2,8 @@ import { WeatherDTO } from "../dto/weather.dto";
 
 export const WeatherAPI = {
   get: async (): Promise<WeatherDTO> => {
-    const endpoint = `${process.env.HOST}/api`;
-    const res = await fetch(endpoint);
+    const endpoint = `${process.env.HOST}/weather`;
+    const res = await fetch(endpoint, { method: "GET" });
     return await res.json();
   },
 };
